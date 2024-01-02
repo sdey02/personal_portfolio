5c5cba7b128dd51988441365f6e34b8a5498c2af
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-02 15:35:00 -0700

    Working on about me section.

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className='text-white'>Hello, I am Shreyans.</h1>
+    </section>
+  )
+}
+
+export default HeroSection
