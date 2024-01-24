0dae814204556ca5db91ae47056cde7e3709f205
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-24 17:27:14 -0500

    Pre pcitures mostly final. Trying to get email portion to work.

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,81 +1,110 @@
-import React from 'react'
-import ProjectCard from './ProjectCard';
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
-    {
-      id: 1,
-      title: "React Portfolio Website",
-      description: "Project 1 description",
-      image: "/images/projects/1.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 2,
-      title: "Potography Portfolio Website",
-      description: "Project 2 description",
-      image: "public/images/projects/1.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 3,
-      title: "E-commerce Application",
-      description: "Project 3 description",
-      image: "public/images/projects/1.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 4,
-      title: "Food Ordering Application",
-      description: "Project 4 description",
-      image: "/images/projects/4.png",
-      tag: ["All", "Mobile"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 5,
-      title: "React Fire base Template",
-      description: "Authentication and CRUD operations",
-      image: "/images/projects/5.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 6,
-      title: "Full-stack Roadmap",
-      description: "Project 5 description",
-      image: "/images/projects/6.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Chip-8 Emulator",
+    description: "A simple chip-8 emulator implementation using c++ and sdl.",
+    image: "/images/projects/1.png",
+    tag: ["All", "C++"],
+    gitUrl: "https://github.com/sdey02/Chip_8_Emulator",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Ray_Tracing_Software",
+    description: "A C++ implementation of a ray tracer.",
+    image: "/images/projects/2.png",
+    tag: ["All", "C++"],
+    gitUrl: "https://github.com/sdey02/Ray_Tracing_Software",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Trading_Bot",
+    description: "A trading bot built using Oanda and Python.",
+    image: "/images/projects/3.png",
+    tag: ["All", "Python"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "TMMC_Vision_Challenge_Software",
+    description: "Our code for the Toyota Vision challenge, where we trained a vision model to identify stickers and holes.",
+    image: "/images/projects/4.png",
+    tag: ["All", "Python"],
+    gitUrl: "https://github.com/sdey02/TMMC_Challenge_Software",
+    previewUrl: "/",
+  },
+];
 
-const ProjectSection = () => {
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
   return (
-    <>  
-        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-1'>My Projects</h2>
-        <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-            {projectsData.map((project) => ( 
-                <ProjectCard 
-                    key={project.id} 
-                    title={project.title} 
-                    description={project.description} 
-                    imgUrl={project.url} 
-                    tag={project}
-                />
-            ))}
-        </div>
-    </>
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="C++"
+          isSelected={tag === "C++"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Python"
+          isSelected={tag === "Python"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
-export default ProjectSection
+export default ProjectsSection;
