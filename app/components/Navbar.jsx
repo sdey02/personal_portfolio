dbf4998601f4448ef47581ae5084ced1341c8d50
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-24 17:37:13 -0500

    Some vercel adjustments

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-
+import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
@@ -50,6 +50,11 @@ const Navbar = () => {
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
