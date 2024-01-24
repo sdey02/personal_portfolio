e37d80f29579ac27ee3f226cc782665a7c57893f
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-24 16:23:52 -0500

    Trying to build on verecel

--- app/components/Navlink.jsx
+++ app/components/Navlink.jsx
@@ -1,14 +1,14 @@
-import { defaultConfig } from "next/dist/server/config-shared"
-import Link from "next/link"
+import Link from "next/link";
 
-const Navlink = ({href, title}) => {
-    return(
-        <Link 
-        href={href} 
-        className='block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-white'>
-            {title}
-        </Link>
-    )
-}
+const NavLink = ({ href, title }) => {
+  return (
+    <Link
+      href={href}
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+    >
+      {title}
+    </Link>
+  );
+};
 
-export default Navlink 
+export default NavLink;
